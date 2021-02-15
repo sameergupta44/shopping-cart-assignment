946acc2891bffdb91afd25af8cda8ff92daa3619
Sameer Gupta <sameer_gupta675@yahoo.com>
2021-02-16 02:15:56 +0530

    + product category filter logic

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -3,10 +3,11 @@ import { Store } from '@ngrx/store';
 import * as ProductActions from '../../reducers/products/products.action';
 import * as CartActions from '../../reducers/cart/cart.action';
 import * as fromRoot from '../../reducers/index';
-import { Observable } from 'rxjs';
+import { Observable, combineLatest } from 'rxjs';
 import { ProductItem } from 'src/app/interface/ProductItem';
 import { CartItem } from 'src/app/interface/CartItem';
 import { Category } from 'src/app/interface/Category';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-products',
@@ -15,17 +16,30 @@ import { Category } from 'src/app/interface/Category';
 })
 export class ProductsComponent implements OnInit {
 
-  productsData$: Observable<any>;
+  // productsData$: Observable<any>;
   categoriesData$: Observable<any>;
   filterData$: Observable<any>;
   cartData$: Observable<any>;
 
+  productsFilteredData$: Observable<any>;
+
   constructor(private store: Store<fromRoot.State>) {
-    this.productsData$ = this.store.select(fromRoot.getProductDataSelector);
+    // this.productsData$ = this.store.select(fromRoot.getProductDataSelector);
     this.cartData$ = this.store.select(fromRoot.getCartItemsSelector);
     this.categoriesData$ = this.store.select(fromRoot.getCategoryDataSelector);
     this.filterData$ = this.store.select(fromRoot.getFilterDataSelector);
-   }
+
+    this.productsFilteredData$ = combineLatest([
+      this.store.select(fromRoot.getProductDataSelector),
+      this.store.select(fromRoot.getFilterDataSelector)]).pipe(
+        map( ([products, filterArry]) => {
+          if (filterArry.length < 1) {
+            return products;
+          }
+          return products.filter( o => filterArry.some(itm => itm.id === o.category));
+        })
+      );
+  }
 
   ngOnInit(): void {
     this.store.dispatch(ProductActions.LoadProductResults());
